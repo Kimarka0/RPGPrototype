7f38dd30204f447cbbc4b8b7f50edb47883914b4
Kimarka0 <frozen11ooooo@gmail.com>
2025-12-23 14:24:49 +0300

    Сделал полноценный инвентарь с перемещением предметов
    Также убрал белые полосы с камер и сделал меню со страницами и перенес туда квесты

--- Assets/Scripts/ItemDragHandler.cs
+++ Assets/Scripts/ItemDragHandler.cs
@@ -3,16 +3,23 @@ using UnityEngine.EventSystems;
 
 public class ItemDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
+    private RectTransform rectTransform;
     private Transform originalParent;
+    private Vector2 originalAnchoredPos;
     private CanvasGroup canvasGroup;
+    private Canvas canvas;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        rectTransform = GetComponent<RectTransform>();
         canvasGroup = GetComponent<CanvasGroup>();
+        canvas = GetComponentInParent<Canvas>();
     }
     public void OnBeginDrag(PointerEventData eventData)
     {
         originalParent = transform.parent;
+        originalAnchoredPos = rectTransform.anchoredPosition;
+        Debug.Log("взят");
         transform.SetParent(transform.root);
         canvasGroup.blocksRaycasts = false;
         canvasGroup.alpha = 0.6f;
@@ -20,7 +27,7 @@ public class ItemDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, I
 
     public void OnDrag(PointerEventData eventData)
     {
-        transform.position = eventData.position;
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
     }
 
     void IEndDragHandler.OnEndDrag(PointerEventData eventData)
@@ -28,6 +35,14 @@ public class ItemDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, I
         canvasGroup.blocksRaycasts = true;
         canvasGroup.alpha = 1f;
         Slot dropSlot = eventData.pointerEnter?.GetComponent<Slot>();
+        if(dropSlot == null)
+        {
+            GameObject dropItem = eventData.pointerEnter;
+            if(dropItem != null)
+            {
+                dropSlot = dropItem.GetComponentInParent<Slot>();
+            }
+        }
         Slot originalSlot = originalParent.GetComponent<Slot>();
 
         if(dropSlot != null)
@@ -36,8 +51,20 @@ public class ItemDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, I
             {
                 dropSlot.currentItem.transform.SetParent(originalSlot.transform);
                 originalSlot.currentItem = dropSlot.currentItem;
+                dropSlot.currentItem.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
             }
+            else
+            {
+                originalSlot.currentItem = null;
+            }
+            transform.SetParent(dropSlot.transform);
+            dropSlot.currentItem = gameObject;
         }
+        else
+        {
+            transform.SetParent(originalParent);
+        }
+        GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
     }
 
 }
